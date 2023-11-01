883026f3f51dfaeb91959eda1f2366d6d677c717
JagdishNagar1501 <nagarjagdish.k@gmail.com>
2023-11-01 17:20:55 +0530

    2_day Complit.

--- 2_day.py
+++ 2_day.py
@@ -15,3 +15,34 @@ print(R)
 #print(type(P))
 #print(type(Q))
 #print(type(R))
+
+#Python Casting
+
+#  Int
+#P = int(1)
+#Q = int(2.8)
+#R = int(3)
+#print(P)
+#print(Q)
+#print(R)
+
+#  Float
+#P = float(1)
+#Q = float(2.5)
+#R = float("3")
+#W = float("4.2")
+#print(P)
+#print(Q)
+#print(R)
+#print(W)
+
+#  String
+#P = str("s1")
+#Q = str(2)
+#R = str(3.0)
+#print(P)
+#print(Q)
+#print(R)
+
+#print(dir(P))
+#print(help(P))
