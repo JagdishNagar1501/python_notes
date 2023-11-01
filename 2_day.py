5b15c7fcbc37d346a47e69868338e3393e29ef97
JagdishNagar1501 <nagarjagdish.k@gmail.com>
2023-11-01 16:50:03 +0530

    check in print statment

---
+++ 2_day.py
@@ -0,0 +1,17 @@
+#Python numbers
+#There are three numeric types in python
+
+P = 2
+Q = 3.5
+R = 1j
+
+# P = Q = R = 1
+#P,Q,R = 1,2,"join"
+
+print(P)
+print(Q)
+print(R)
+
+#print(type(P))
+#print(type(Q))
+#print(type(R))
