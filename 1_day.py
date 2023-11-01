fd0d7329e3c5d5d93f97d982e0aca33ab91890bd
JagdishNagar1501 <nagarjagdish.k@gmail.com>
2023-11-01 14:09:56 +0530

    complit

--- 1_day.py
+++ 1_day.py
@@ -9,4 +9,32 @@
 #2. what are the features of python?
 #Ans:
 #  python is a object-oriented programming language that is GUI programming language , easy to 
-#   learn, easy to read, easy to maintain, protable ,databases 
+#   learn, easy to read, easy to maintain, protable ,databases.
+
+#3. How we can execute code in python?
+#Ans:
+#  Interactive mode : The way to run a python in interactive mode.
+#                  -- type program statement.
+
+# Script mode : The way to run a python file 
+#            -- python welcome.py
+
+#4. What are reserved word in python?
+#Ans:
+# there many reserved word : print, pass, main , if ,and ,assert ,break , for, def, del, elif,
+#                            else, except, is, while, with, try, import, or, not.
+
+#5. What is indentation?
+#Ans:
+#       -- Blocks of code are denoted by line indentation.
+#		-- All statements within the block must be indented the same amount.
+
+#6. How many types of comments?
+#Ans:
+#       -- Comments start with a # (One Line).   
+
+#7. What is doc-string?
+#Ans:
+#       -- Python also has extended documentation capability, called docstrings.
+#		-- Docstrings can be one line, or multiline.
+#		-- Python uses triple quotes at the beginning and end of the docstring.
