a0dd47cc8769cce1e0d8f582bc9c709f69f77553
JagdishNagar1501 <nagarjagdish.k@gmail.com>
2023-11-01 18:05:09 +0530

    question is complit

--- 2_day.py
+++ 2_day.py
@@ -49,10 +49,12 @@ print(R)
 
 
 #Git Interview Question.
+
 #1. What is version control?
 #Ans:
 #    Version control systems are a category of software tools that helps in recording 
 #    changes made to files by keeping a track of modifications done in the code.
+
 
 #2. What is git?
 #Ans:
@@ -60,6 +62,27 @@ print(R)
 #   2.Git is a DevOps tool used for source code management.
 #   3.Git is an open-source distributed version control system.
 
+
 #3. What is difference between git add and commit?
 #Ans:
-#   
+#   git add adds your modified files to the queue to be committed later. Files are not committed
+
+#   Commits are the core building block units of a Git project timeline.
+
+
+#4. What is difference between git push and pull?
+#Ans:
+#   The git push command is used to upload content to a remote repository.
+
+#   The git pull command is used to fetch and download content from a remote repository and 
+#   immediately update the local repository to match that content.
+
+
+#5. What is git merge and pull request(PR)?
+#Ans:
+#      Merge:  Merge is Git's way of putting a forked history back together again. 
+#   The git merge command lets you take the independent lines of development created 
+#   by git branch and integrate them into a single branch.
+
+#   Pull request: pull requests are a mechanism for a developer to notify team members that 
+#   they have completed a feature.
