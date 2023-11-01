fdd33335924400d0866a49fdeae4cd30b7356062
JagdishNagar1501 <nagarjagdish.k@gmail.com>
2023-11-01 12:51:54 +0530

    interview

--- 1_day.py
+++ 1_day.py
@@ -1,4 +1,12 @@
 #1. Why we use python?
-#ans:   python is an interpreted, object-oriented, high-level language with dynamic semantic.
+#Ans:  
+#  python is an interpreted, object-oriented, high-level language with dynamic semantic.
+
 # python is a popular programming language.it was created by guido van rossum,released in 1991.
+
 # it used for web development, software development, GUI development, mathematics, system scripting.
+
+#2. what are the features of python?
+#Ans:
+#  python is a object-oriented programming language that is GUI programming language , easy to 
+#   learn, easy to read, easy to maintain, protable ,databases 
