27c0959c5ff6a79a655dda09c08867bb667fca54
JagdishNagar1501 <nagarjagdish.k@gmail.com>
2023-11-01 12:23:00 +0530

    interview prepration

--- 1_day.py
+++ 1_day.py
@@ -1,1 +1,4 @@
-print("hello world.")
+#1. Why we use python?
+#ans:   python is an interpreted, object-oriented, high-level language with dynamic semantic.
+# python is a popular programming language.it was created by guido van rossum,released in 1991.
+# it used for web development, software development, GUI development, mathematics, system scripting.
