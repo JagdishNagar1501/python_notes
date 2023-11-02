c7b23868823d79119c008f7bf4ff630444870b07
JagdishNagar1501 <nagarjagdish.k@gmail.com>
2023-11-02 12:06:39 +0530

    day_4 complit

---
+++ 4_day.py
@@ -0,0 +1,19 @@
+# --:: Python Operators
+# Operators are used to perform operations on variables and values.
+# Python divides the operators in the following groups:
+
+# Arithmetic operators ( +, -, *, /, %, //, **)
+# Assignment operators (=, +=)
+# Comparison operators (==, !=, >, <, >=, <=)
+
+# Logical operators (and, or, not)
+# Identity operators (is, is not, )
+# Membership operators (in, not in)
+# Bitwise operators (&, |, ^, <<, >>)
+
+# print(abs(-12.12))
+
+# print(round(3.75))
+# print(round(3.15))
+# print(round(3.16, 1)) #round digit
+
