f0fab564839adc7c9b2fabc015256d780b08630e
JagdishNagar1501 <nagarjagdish.k@gmail.com>
2023-11-07 18:36:09 +0530

    task_6 incomplited

---
+++ task_6.py
@@ -0,0 +1,16 @@
+data = {}
+question = input('Enter the Question: ')
+data["question"] = question
+Option_1 = input("Enter the Option A:")
+Option_2 = input("Enter the Option B:")
+Option_3 = input("Enter the Option C:")
+Option_4 = input("Enter the Option D:")
+data["Option"] = {'A:', "B:", "C:", "D:"}
+ans = input("Enter the Answer:")
+data["ans"] = ans
+print(data)
+
+# print("Option :--") 
+# print('A :',date['Enter the Option A:'],'\t\tB :',date['Enter the Option B:'])
+# print('C :',date['Enter the Option C:'],'\tD :',date['Enter the Option D:'])
+
