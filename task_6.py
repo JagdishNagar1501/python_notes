b33d3924f647ad5075a5b901376a584d1894414c
JagdishNagar1501 <nagarjagdish.k@gmail.com>
2023-11-08 16:30:05 +0530

    task

--- task_6.py
+++ task_6.py
@@ -1,36 +1,39 @@
 data = {}
+while(True):
+    print("1.Enter the Question:-")
+    print("2.Enter the Answer:-")
+    print("3.Exit")
+    user_choice = input("What is your choice ? :-")
+    if int(user_choice) == 1: 
+        question = input('Enter the Question: ')
+        data["question"] = question
+        Option_1 = input("Enter the Option A:")
+        data["A"] = Option_1
+        Option_2 = input("Enter the Option B:")
+        data["B"] = Option_2
+        Option_3 = input("Enter the Option C:")
+        data["C"] = Option_3
+        Option_4 = input("Enter the Option D:")
+        data["D"] = Option_4
+        ans = input("Enter the Answer:")
+        data["ans"] = ans
 
-print("1.Enter the Question:-")
-print("2.Enter the Answer:-")
+    elif int(user_choice) == 2:
+        print("Question :-")
+        print(data["question"])
+        print("Option :-")
+        print('A :',data['A'],'\t\tB :',data['B'])
+        print('C :',data['C'],'\t\tD :',data['D'])
 
-user_choice = input("What is your choice ? :-")
-if int(user_choice) == 1:
-    question = input('Enter the Question: ')
-    data["question"] = question
-    Option_1 = input("Enter the Option A:")
-    data["A"] = Option_1
-    Option_2 = input("Enter the Option B:")
-    data["B"] = Option_2
-    Option_3 = input("Enter the Option C:")
-    data["C"] = Option_3
-    Option_4 = input("Enter the Option D:")
-    data["D"] = Option_4
-    ans = input("Enter the Answer:")
-    data["ans"] = ans
-
-elif int(user_choice) == 2:
-    print("Option :-")
-    print('A :',data['A'],'\t\tB :',data['B'])
-    print('C :',data['C'],'\t\tD :',data['D'])
-
-    user_ans = input("Enter your ans: ")
-    if user_ans==data['ans']:
-        print("Anser is True")
+        user_ans = input("Enter your ans: ")
+        if user_ans==data['ans']:
+            print("Anser is True")
+        else:
+            print("Anwer is False")
+    elif int(user_choice) == 3:
+        break
     else:
-        print("Anwer is False")
+        print("Invalid Input")
 
-else:
-    print("Invalid Input")
 
-# #start Qiuz
 
---
+++ while_demo.py
@@ -0,0 +1,14 @@
+# while(True):
+#     age = int(input("Enter your age: "))
+#     if age > 25:
+#         print(f"your age is: {age}")
+#     else:
+#         break
+
+# license = input("Enter license: ")
+# while(license == "yes"):
+#     age = int(input("Enter your age: "))
+#     if age > 25:
+#         print(f"your age is: {age}")
+#     else:
+#         license = "no"
